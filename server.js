24e579e90be1e06d05b6dddb898062ee4de2002f
Michael Delcore <nmdelcore@gmail.com>
2018-02-28 13:00:11 -0700

    fixex file structure

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ app.use(bodyParser.json({type:'application/vnd.api+json'}));
 //Routing
 //=============================================================
 
-require('./app/routing/api-routes.js')(app); 
-require('./app/routing/html-routes.js')(app);
+require('./routing/api-routes.js')(app); 
+require('./routing/html-routes.js')(app);
 
 
 // =============================================================
